dad6b8e1820037e2d08a3dedab0cb003ca389535
binzz-xploit <binzzindo@gmail.com>
2025-11-22 01:22:38 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ©AiiSigma
+
+module.exports = {
+  domain: "https://prfif.pterodaytl.my.id", // Ganti sma domain panel lu dan pastiin harus *http*
+  port: "40" // Ganti Portnoy di panel network.
+};
